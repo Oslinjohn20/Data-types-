6e8c7f98a46a6b4809a326119ac3d3947ffe7afc
oslin <ossiekcjohnson@gmail.com>
2020-03-10 10:12:35 +0200

    new data type exercise

---
+++ data type.py
@@ -0,0 +1,46 @@
+# declaring a string of variables x
+x = "Life's good you know"
+# displaying data type of s'
+print(type(x))
+# extracting a word from original statement
+print(x[5:6:7])
+
+# declaring variables as integers
+u= 10j
+y= 5
+z= 5.8
+# displaying of data types
+print(type(y), type(z), type(u))
+
+# declaring variables to integers and string
+mylist= [23,82,42,84,"Andrew"]
+# displaying the data types
+print(type(mylist))
+
+my_list=[23,82,42,84,33]
+print(type(my_list))
+
+# inserting a int in a position
+my_list.insert(4,23)
+print(my_list)
+
+# reversing a variable
+my_list.reverse()
+print (my_list)
+
+# sorting variables. Showing how the variable will sort it self into ascending order
+my_list.sort()
+print(my_list)
+
+# showing the pop function
+my_list.pop()
+print(my_list)
+
+# declaring variables to a string
+cars= {"Aston Martin","VW","Ford","BMW","Nissan"}
+print(type(cars))
+
+# changing the brackets
+cars1 = ("Ashton Martin","VW","Ford","BMW","Nissan")
+# identifying the  type
+print(type(cars1))
